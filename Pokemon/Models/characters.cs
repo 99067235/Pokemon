090352a6645cbc65354743ed324c8bc57cff3731
99062735 <99067235@mydavinci.nl>
2023-04-20 09:50:39 +0200

    Add project files.

---
+++ Pokemon/Models/characters.cs
@@ -0,0 +1,23 @@
+﻿namespace Pokemon.Models
+{
+    public class Character
+    {
+        public string defaultName { get; set; }
+        public string chosenName { get; set; }
+        public string strength { get; set; }
+        public string weakness { get; set; }
+
+        public Character(string defaultName, string chosenName, string strength, string weakness)
+        {
+            this.defaultName = defaultName;
+            this.chosenName = chosenName;
+            this.strength = strength;
+            this.weakness = weakness;
+        }
+
+        public override string ToString()
+        {
+            return $"DefaultName: {defaultName}, ChosenName: {chosenName}, Strength: {strength}, Weakness: {weakness}";
+        }
+    }
+}
---
+++ Pokemon/Program.cs
@@ -0,0 +1,14 @@
+﻿using Pokemon.Models;
+
+Character henk = new Character("henk", "henk", "fire", "water");
+Console.WriteLine(henk.ToString());
+
+Console.Write("Name: ");
+string chosenName = Console.ReadLine();
+henk.chosenName = chosenName;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(chosenName);
+}
+Console.WriteLine(henk);
