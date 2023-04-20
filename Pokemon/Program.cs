6a81eb07025054e88efc2472d7cb28b0520d4ce1
99062735 <99067235@mydavinci.nl>
2023-04-20 10:21:41 +0200

    Assignment 1 finished.

--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -1,14 +1,21 @@
 ﻿using Pokemon.Models;
 
 Character henk = new Character("henk", "henk", "fire", "water");
-Console.WriteLine(henk.ToString());
 
-Console.Write("Name: ");
-string chosenName = Console.ReadLine();
-henk.chosenName = chosenName;
-
-for (int i = 0; i < 10; i++)
+while (true)
 {
-    Console.WriteLine(chosenName);
+    Console.Write("Name: ");
+    string chosenName = Console.ReadLine();
+    if (chosenName == "exit")
+    {
+        break;
+    } else
+    {
+        henk.chosenName = chosenName;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(chosenName + '!');
+        }
+    }
 }
-Console.WriteLine(henk);
