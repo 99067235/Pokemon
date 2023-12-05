345e92e354032da79d93af54c4b54cbb2a0ffa61
99062735 <99067235@mydavinci.nl>
2023-12-05 13:27:22 +0100

    Moved the battle logic from the main method to a seperate battle class.

--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -46,49 +46,6 @@ internal partial class Program
                 trainer2.belt.add(trainer2.belt.makePokeball(new Charmander()));
             }
         }
-        // Start the battle
-        // Define the variables which keep track of the amount of wins
-        int winsTrainer1 = 0;
-        int winsTrainer2 = 0;
-        // Define a variable to keep track of how many ties there are played
-        int ties = 0;
-        while (true)
-        {
-            try
-            {
-                var trainer1Pokemon = trainer1.belt.getItemFromBelt(winsTrainer1);
-                var trainer2Pokemon = trainer2.belt.getItemFromBelt(winsTrainer2);
-                Trainer winner = Battle.StartBattle(trainer1, trainer2, trainer1Pokemon, trainer2Pokemon);
-                if (winner != null)
-                {
-                    if (winner == trainer1)
-                    {
-                        winsTrainer1++;
-                    } else
-                    {
-                        winsTrainer2++;
-                    }
-                } else
-                {
-                    ties++;
-                }
-            } catch
-            {
-                break;
-            }
-        }
-        Console.WriteLine("Results: ");
-        if (winsTrainer1 > winsTrainer2)
-        {
-            Console.WriteLine($"{trainer1.name} has won from {trainer2.name} with a score of {winsTrainer1}");
-        }
-        else if (winsTrainer2 > winsTrainer1)
-        {
-            Console.WriteLine($"{trainer2.name} has won from {trainer1.name} with a score of {winsTrainer2}");
-        }
-        else
-        {
-            Console.WriteLine($"It's a tie, you both had {winsTrainer1} wins.");
-        }
+        Battle.HandleBattle(trainer1, trainer2);
     }
 }
